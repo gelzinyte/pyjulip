58cdf60f48d03eef3c6048de1f3c8fd71be4c6a8
casv2 <casv2@cam.ac.uk>
2019-07-16 23:07:46 +0100

    update

--- example/test.py
+++ example/test.py
@@ -3,6 +3,12 @@ import pyjulip
 from ase.lattice.cubic import BodyCenteredCubic
 from ase.lattice.cubic import Diamond
 
+def test(at, pot):
+    for i in xrange(10):
+        at.rattle(1E-4)
+        at.set_calculator(pot)
+        print at.get_potential_energy()
+
 ####
 
 pot = pyjulip.NBodyIPs("Ti_aPIP1_reg.json", fast=True)
@@ -12,10 +18,7 @@ print "NBODYIPS"
 ti_at = BodyCenteredCubic("Ti", latticeconstant=3.32)
 ti_big_at = ti_at * (2,2,2)
 
-for i in xrange(10):
-    ti_big_at.rattle(1E-4)
-    ti_big_at.set_calculator(pot)
-    print ti_big_at.get_potential_energy()
+test(si_big_at, pot)
 
 ######
 
@@ -26,11 +29,7 @@ si_big_at = si_at * (2,2,2)
 
 print "StillingerWeber()"
 
-for i in xrange(10):
-    si_big_at.rattle(1E-4)
-    si_big_at.set_calculator(pot2)
-    print si_big_at.get_potential_energy()
-
+test(si_big_at, pot2)
 
 ######
 
@@ -41,21 +40,10 @@ w_big_at = w_at * (2,2,2)
 
 print "W_EAM"
 
-for i in xrange(10):
-    w_big_at.rattle(1E-4)
-    w_big_at.set_calculator(pot3)
-    print w_big_at.get_potential_energy()
+test(w_big_at, pot3)
 
 ######
 
 pot4 = pyjulip.FinnisSinclair("../data/W-pair-Wang-2014.plt", "../data/W-e-dens-Wang-2014.plt")
 
-w_at = BodyCenteredCubic("W", latticeconstant=3.16)
-w_big_at = w_at * (2,2,2)
-
-print "W_FS"
-
-for i in xrange(10):
-    w_big_at.rattle(1E-4)
-    w_big_at.set_calculator(pot4)
-    print w_big_at.get_potential_energy()
+test(w_big_at, pot4)
