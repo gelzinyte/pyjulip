eb59d6c4607bba589d789988ad9d82a5d580a7a5
casv2 <casv2@cam.ac.uk>
2020-06-30 23:27:51 +0200

    IP.components[3]

--- pyjulip.py
+++ pyjulip.py
@@ -32,7 +32,7 @@ def SHIP(potname):
     julia.using("SHIPs")
     julia.eval("D = load_dict(\"" + potname + "\")")
     julia.eval("IP = read_dict(D[\"IP\"])")
-    julia.eval("IPf = SHIPs.graph_evaluator(IP)")
+    julia.eval("IP.components[3] = SHIPs.graph_evaluator(IP.components[3])")
     ASE_IP = JulipCalculator("IP")
     return ASE_IP
 
